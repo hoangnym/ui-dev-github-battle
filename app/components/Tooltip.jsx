413a5c30c9a2c774dc101612d7f715bb365ad966
hoangnym <hoangnym94@gmail.com>
2022-11-06 10:25:17 +0100

    replace higher order component with render props

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,30 +1,44 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import withHover from "./withHover"
+// import withHover from "./withHover";
+import Hover from "./Hover";
 
 const container = {
   position: "relative",
-  display: "flex"
+  display: "flex",
+};
 
-}
-
-class Tooltip extends React.Component {
-
-  render() {
-    const { children, element, hovering } = this.props
-    
-    return (
-      <div style={container}>
-        {hovering === true && element}
-        {children}
-      </div>
-    )
-  }
+export default function Tooltip({ children, element }) {
+  return (
+    <Hover>
+      {(hovering) => {
+        return (
+          <div style={container}>
+            {hovering === true && element}
+            {children}
+          </div>
+        );
+      }}
+    </Hover>
+  );
 }
 
 Tooltip.propTypes = {
   children: PropTypes.node.isRequired,
-  element: PropTypes.node.isRequired
-}
+  element: PropTypes.node.isRequired,
+};
 
-export default withHover(Tooltip)
+// class Tooltip extends React.Component {
+//   render() {
+//     const { children, element, hovering } = this.props;
+
+//     return (
+//       <div style={container}>
+//         {hovering === true && element}
+//         {children}
+//       </div>
+//     );
+//   }
+// }
+
+// export default withHover(Tooltip);
