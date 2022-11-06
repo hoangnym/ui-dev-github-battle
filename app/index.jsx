960108f755477b3948974739904021b30a8e0c29
hoangnym <hoangnym94@gmail.com>
2022-11-06 13:26:13 +0100

    add react-router-dom

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,27 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import "./index.css"
-import Popular from './components/Popular'
-import Battle from './components/Battle'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import Popular from "./components/Popular";
+import Battle from "./components/Battle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className='light'>
-                <div className='container'>
-                    <Popular />
-                    {/* <Battle /> */}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <div className="light">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Popular />} />
+              <Route path="/battle" element={<Battle />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
-const rootElement = document.getElementById('app')
-const root = ReactDOM.createRoot(rootElement)
-root.render(<App />)
+const rootElement = document.getElementById("app");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
